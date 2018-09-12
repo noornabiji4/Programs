bbbc877c79344962a0aebdfe2ab17958c43bcc1f
Nabiji Noor <noornabiji4@gmail.com>
2018-09-13 01:35:42 +0530

    calculating each value of string

---
+++ algoritms/countStringChar.js
@@ -0,0 +1,18 @@
+//Write a function which takes in a string and return the count of each charecter in string 
+
+
+    function stringCount(string){
+        var object={};
+        var i="";
+        for (i=0; i<string.length; i++){
+            let count =string[i];
+            if (object[count]>0){
+                object[count]++;
+            }
+            else {
+                object[count]=1;
+            }
+        }
+        return object;
+    }
+    stringCount("hello");
